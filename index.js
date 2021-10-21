eb0d48213719c437a0a0923f9825a165ff1d70dc
Harry <hrgus3@gmail.com>
2021-10-21 18:43:25 -0400

    commit tree f54728424823915c9da4a685fb8c68e8d82e1aa8

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(distanceInBlocks){             //returns a distance in blocks
+    if (distanceInBlocks > 42) {
+        return (distanceInBlocks - 42)
+    }  else  
+        return (42 - distanceInBlocks)  
+}
+distanceFromHqInBlocks(43)
+
+function distanceFromHqInFeet(distanceTravelledInFeet){
+    distanceFromHqInBlocks();                                        // (38-34)*264
+    
+}
+distanceFromHqInBlocks(43)
+
+function distanceTravelledInFeet(){
+
+}
+
+
+function calculatesFarePrice(){
+
+}
