b632e4afb29f1ef75f8974360677747c1828a0e2
Harry <hrgus3@gmail.com>
2021-10-22 15:37:12 -0400

    Not completed

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(distanceInBlocks){             //returns a distance in blocks
+    if (distanceInBlocks > 42) {
+        return (distanceInBlocks - 42)
+    }  else  
+        return (42 - distanceInBlocks)  
+}
+distanceFromHqInBlocks(43)
+
+function distanceFromHqInFeet(){
+    distanceFromHqInBlocks();                                        // (38-34)*264
+    return 
+}
+distanceFromHqInBlocks(43)
+
+function distanceTravelledInFeet(){
+
+}
+
+
+function calculatesFarePrice(){
+
+}
